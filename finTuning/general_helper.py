0bae6ad2c7b143631931eea24621c6849804b9c9
bestiesbesties <leyton.ms@icloud.com>
2026-05-25 20:21:50 +0200

    remove label_meta from config

--- finTuning/general_helper.py
+++ finTuning/general_helper.py
@@ -5,5 +5,4 @@ def load_config():
     assert os.path.exists("config.json")
     with open("config.json", "r") as f:
         config = json.load(f)
-    config["label_meta"]["id2label"] = {int(k): v for k, v in config["label_meta"]["id2label"].items()}
     return config
