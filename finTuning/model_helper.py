56fc5c550cfef530cd6364f589a5fcc71e5d2fa0
bestiesbesties <leyton.ms@icloud.com>
2026-05-25 20:22:40 +0200

    print num_label & save model to built

--- finTuning/model_helper.py
+++ finTuning/model_helper.py
@@ -1,19 +1,24 @@
+import os
 from transformers import AutoModelForTokenClassification, AutoTokenizer
 
 def load_model(path:str, labels:list, reinitialize=False):
     tokenizer = AutoTokenizer.from_pretrained(path)
     ## TODO use RobertalForTokenClassification.from_pretrained?
     if reinitialize: ## TODO add '.' to pathstring?
-        model = AutoModelForTokenClassification.from_pretrained(path, num_labels=len(labels) , ignore_mismatched_sizes=True)
+        num_labels = len(labels)
+        print("re-initializing, num_labels: ", num_labels)
+        model = AutoModelForTokenClassification.from_pretrained(path, num_labels=num_labels , ignore_mismatched_sizes=True)
         model = _update_config(model, labels)
         model = _freeze_unfreeze(model, 3)
     else:
         model = AutoModelForTokenClassification.from_pretrained(path)
     return tokenizer, model
 
-def save_model(path, tokenizer, trainer):
-    tokenizer.save_pretrained("./models/ner_finetuned")
-    trainer.save_model("./models/ner_finetuned")
+def save_model(name:str, batch:str, tokenizer, trainer): ## TODO runtimeWarning upon exist
+    fp = os.path.join("models", "built", f"{name}_{batch}") ## TODO mkdir
+    print("fp: ", fp)
+    tokenizer.save_pretrained(fp)
+    trainer.save_model(fp)
 
 def _update_config(model, labels:list):
     print("hidden_size", model.config.hidden_size)
