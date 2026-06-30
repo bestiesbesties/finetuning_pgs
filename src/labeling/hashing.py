c2bedc4ec1ca372b85795a5e51fb4bc6aeed87b0
bestiesbesties <leyton.ms@icloud.com>
2026-06-30 12:53:21 +0200

    add text next to text digest in sqlite

--- src/labeling/hashing.py
+++ src/labeling/hashing.py
@@ -3,55 +3,55 @@ import hashlib
 import sqlite3
 import ast
 import json
-from .serialization import to_df
+from .serialization import to_df, to_jsonl
 
-from src.labeling.serialization import to_df, to_jsonl
+# from src.labeling.serialization import to_df, to_jsonl
 
 conn = sqlite3.connect("hash.db")
 cur = conn.cursor()
 cur.execute("""
-    CREATE TABLE IF NOT EXISTS hashtable (
+    CREATE TABLE IF NOT EXISTS hashtable2 (
         hash TEXT PRIMARY KEY,
-        pred TEXT
+        pred TEXT,
+        text TEXT
     )
 """)
 
 def _upsert(text, pred):
-    hash = hashlib.sha256(text.encode("utf-8")).hexdigest()
-    try:
-        cur.execute("INSERT INTO hashtable (hash, pred) VALUES (?, ?)", [hash, json.dumps(pred)])
-        conn.commit()
-        return True
-    except sqlite3.IntegrityError:
-        return False
+    hash_value = hashlib.sha256(text.encode("utf-8")).hexdigest()
+    cur.execute("""
+        INSERT INTO hashtable2 (hash, text, pred)
+        VALUES (?, ?, ?)
+        ON CONFLICT(hash) DO UPDATE SET
+            text = excluded.text,
+            pred = excluded.pred
+    """, (hash_value, text, json.dumps(pred)))
+    conn.commit()
+    return True
+
+def upsert_df(df):
+    for i, v in df.iterrows():
+        _upsert(v["text"], v["pred"])
+    return df
 
 def _check(text):
     cur.execute(
-        "SELECT pred FROM hashtable WHERE hash = ? LIMIT 1",
+        "SELECT pred FROM hashtable2 WHERE hash = ? LIMIT 1",
         [hashlib.sha256(text.encode("utf-8")).hexdigest()]
     )
     row = cur.fetchone()
-    return row
+    if row:
+        return ast.literal_eval(row[0]) 
+    else:
+        return None
 
-def upsert_jsonl(path):
-    df = to_df(path)
-    for i, v in df.iterrows():
-        _upsert(v["text"], v["pred"])
-
-def check_jsonl(path):
-    df = to_df(path)
-    print(df)
-
+def check_df(df):
+    # df.to_csv("temp_.csv")
+    df["changed"] = False
     for i, v in df.iterrows():
         row = _check(v["text"])
         if row is not None:
-            x = json.loads(row[0])
-            y = df.loc[i, "pred"] 
-            if x != y:
-                print("updating: ", i)
-                df.loc[i, "pred"] =  x
-            
-    to_jsonl(df, path)
-
-# upsert_jsonl("adminultra.jsonl")
-check_jsonl("main/block_0/run_0/export.jsonl")
+            # print("updating: ", i, row)
+            df.at[i, "pred"] = row
+            df.at[i, "changed"] = True                                                                                                                                                                                              
+    return df
