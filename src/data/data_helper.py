22821cfad7b79b31035b44f070ebcab14ee5a1ce
bestiesbesties <leyton.ms@icloud.com>
2026-06-30 12:52:33 +0200

    added pool transformation pipeline support + fixed double index

--- src/data/data_helper.py
+++ src/data/data_helper.py
@@ -5,7 +5,7 @@ from sklearn.model_selection import train_test_split
 from src.sampeling import confidence, histing
 
 def undersample(path):
-    df = pd.read_csv(path)
+    df = pd.read_csv(path, index_col=0)
     return df.sample(config["block_undersample_size"], random_state=config["uniform_random_state"])
 
 def train_test(df):
@@ -49,26 +49,33 @@ def prune(df):
     df.drop(["chars_length", "words_length"], axis=1, inplace=True)
     return df
 
-def transform_pool(data_path, run_path, n, ch):
-    pool = pd.read_csv(data_path)
-    if ch:
+def transform_pool(data_path, run_path, n, pipeline):
+    pool = pd.read_csv(data_path, index_col=0)
+    pipeline = pipeline.lower() if isinstance(pipeline, str) else ""
+    if pipeline == "c_h" or pipeline == "c_r":
+        print("confidence routing")
         results = pool["text"].apply(lambda x: confidence.route(x, os.path.join(run_path, "model")))
         pool["conf"] = results.str[0]
         pool["pred"] = results.str[1]
-        sample = histing.stratified_sample(pool, n)
-        print("sample: ", sample) ## TODO index
+        if pipeline == "c_h":
+            print("histing")
+            sample = histing.stratified_sample(pool, n)
+        elif pipeline == "c_r":
+            print("random sampeling")
+            sample = pool.sample(n=n, random_state=config["uniform_random_state"])
     else:
+        print("empty random sampeling")
         sample = pool.sample(n=n, random_state=config["uniform_random_state"])
         sample["pred"] = [[] for _ in range(len(sample))]
-    sample.index ## TODO overwrite index
-    pool.to_csv(data_path, index=False) ## overwrite
+    print("dropping: ", sample.index)
+    pool.drop(sample.index, inplace=True)
+    pool.to_csv(data_path, index=True) 
     return sample
 
 def _create_bio_tags(row, tokenizer, model): ## TODO load from global/module dependency inject
     ## !! TODO ambiguous source match code with TPS
-    print("row: ", row)
     text = row["text"]
-    entities = row["label"]
+    entities = row["pred"]
 
     encoding = tokenizer(
         text,
@@ -102,14 +109,11 @@ def _create_bio_tags(row, tokenizer, model): ## TODO load from global/module dep
     return encoding
 
 def tokenize_data(data:list, tokenizer, model): ## TODO load from global/module dependency inject
-    print("Tokenizing")
-    print("type(data): ", type(data))
     print(data.head())
-    print("data.columns", data.columns)
     tokenized_data = [_create_bio_tags(row, tokenizer, model) for _, row in data.iterrows()]
 
-    for i, seq in enumerate(tokenized_data[:2]):
-        print("Index", i, "Value", seq)
-        assert len(seq["input_ids"]) == len(seq["labels"]), f"len mismatch at train {i}"
+    # for i, seq in enumerate(tokenized_data[:2]):
+    #     print("Index", i, "Value", seq)
+    #     assert len(seq["input_ids"]) == len(seq["labels"]), f"len mismatch at train {i}"
 
     return tokenized_data
